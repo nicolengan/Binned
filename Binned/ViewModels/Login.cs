28d0907468d593fd22d18fab47151116f431f69c
lyj4333 <lyj4333@gmail.com>
2022-12-29 18:24:59 +0800

    drafted acc pages

---
+++ Binned/ViewModels/Login.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Binned.ViewModels
+{
+    public class Login
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+
+
+    }
+}
---
+++ Binned/ViewModels/Register.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Binned.ViewModels
+{
+    public class Register
+    {
+
+		[Required]
+		[DataType(DataType.Text)]
+		public string FirstName { get; set; }
+
+		[Required]
+		[DataType(DataType.Text)]
+		public string LastName { get; set; }
+
+		[Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+		[Required]
+		[DataType(DataType.Text)]
+		public string Username { get; set; }
+
+		[Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Password and confirmation password does not match")]
+        public string ConfirmPassword { get; set; }
+	}
+
+}
