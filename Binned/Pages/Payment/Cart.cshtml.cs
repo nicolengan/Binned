ccf0cf43f5f8c2ea301bc37d5099fffee3ff69d8
nic <nicolenganjiaxian@gmail.com>
2023-01-11 00:34:47 +0800

    added product stripe api

--- Binned/Pages/Payment/Cart.cshtml.cs
+++ Binned/Pages/Payment/Cart.cshtml.cs
@@ -6,25 +6,50 @@ namespace Binned.Pages.Payment
 {
     public class CartModel : PageModel
     {
+        private readonly ILogger<CartModel> _logger;
+
+        public CartModel(ILogger<CartModel> logger)
+        {
+            _logger = logger;
+        }
         public void OnGet()
         {
-            var options = new ProductCreateOptions
+            // ideally want to add this everytime a new product is created but we'll jst put it here x(
+            var searchOptions = new ProductSearchOptions
             {
-                Name = "Product 1",
-                DefaultPriceData =
-                {
-                    Currency = "SGD",
-                    UnitAmount = 12,
-                    TaxBehavior = "inclusive"
-                    // maybe add an url and image?
-                },
-                Metadata = new Dictionary<string, string>
-                {
-                    { "ProductId", "1234" },
-                }
+                Query = "active:'true' AND metadata['ProductId']:'1234'",
             };
-            var service = new ProductService();
-            service.Create(options);
+            var searchService = new ProductService();
+            var searched = searchService.Search(searchOptions);
+            var ifNull = searched.FirstOrDefault();
+
+            _logger.LogInformation("search null or not {s}", ifNull);
+
+            if (ifNull == null)
+            {
+                var options = new ProductCreateOptions
+                {
+                    Name = "Product 1",
+                    DefaultPriceData = new ProductDefaultPriceDataOptions
+                    {
+                        Currency = "SGD",
+                        UnitAmount = 1200,
+                        TaxBehavior = "inclusive"
+                        // maybe add an url and image ?
+                    },
+                    Metadata = new Dictionary<string, string>
+                    {
+                        { "ProductId", "1234" },
+                    }
+                };
+                var service = new ProductService();
+                var l = service.Create(options);
+                _logger.LogInformation("hello prod created {l}", l);
+            }
+            else
+            {
+                _logger.LogInformation("hello prod alr created");
+            }
         }
     }
 }
