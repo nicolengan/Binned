7e5fa0185747fc2d7f3b1577b22e4f754ec14138
nic <nicolenganjiaxian@gmail.com>
2023-01-11 16:49:14 +0800

    deleted account files

--- Binned/Pages/Payment/Cart.cshtml.cs
+++ Binned/Pages/Payment/Cart.cshtml.cs
@@ -14,7 +14,10 @@ namespace Binned.Pages.Payment
         }
         public void OnGet()
         {
-            // ideally want to add this everytime a new product is created but we'll jst put it here x(
+            // ideally want to add this everytime a new product is created but we'll jst put it here for now x(
+            // creates product in stripe, finds if it exists or not by product id if not will create product
+            // product id is saved into database (column id) and that is used to search
+            // theres like a few sec delay but shouldnt be a prob when this is added to the add product admin side
             var searchOptions = new ProductSearchOptions
             {
                 Query = "active:'true' AND metadata['ProductId']:'1234'",
@@ -30,10 +33,10 @@ namespace Binned.Pages.Payment
                 var options = new ProductCreateOptions
                 {
                     Name = "Product 1",
-                    DefaultPriceData = new ProductDefaultPriceDataOptions
+                    DefaultPriceData = new ProductDefaultPriceDataOptions // adds price item
                     {
                         Currency = "SGD",
-                        UnitAmount = 1200,
+                        UnitAmount = 1200, // in cents so hv to add 2 more zeros after
                         TaxBehavior = "inclusive"
                         // maybe add an url and image ?
                     },
