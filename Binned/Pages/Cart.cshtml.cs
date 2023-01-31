63b9fac08cf9b35a066f9400eddcb06cbce3d63c
nic <nicolenganjiaxian@gmail.com>
2023-02-01 00:22:17 +0800

    edited

--- Binned/Pages/Cart.cshtml.cs
+++ Binned/Pages/Cart.cshtml.cs
@@ -1,3 +1,4 @@
+using Binned.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,28 @@ namespace Binned.Pages
 {
     public class CartModel : PageModel
     {
-        public void OnGet()
+
+        private readonly CartService _cartService;
+        private readonly ProductService _productService;
+        public CartModel(ProductService productService, CartService cartService)
         {
+            _cartService = cartService;
+            _productService = productService;
+        }
+
+        public Models.Cart Cart { get; set; } = new Models.Cart();
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Cart = await _cartService.GetCartByUserName("test");
+
+            return Page();
+        }
+        public async Task<IActionResult> OnPostRemoveFromCartAsync(int CartItemId)
+        {
+            await _cartService.RemoveItem(CartItemId);
+
+            return RedirectToPage("/Cart");
         }
     }
 }
