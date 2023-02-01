e41380bcc0d3253f9d9fbe9c3d00d4a99d8bab64
lyj4333 <lyj4333@gmail.com>
2023-02-02 01:11:42 +0800

    added roles and merged acc pt 2

--- Binned/Pages/Account/Login.cshtml.cs
+++ Binned/Pages/Account/Login.cshtml.cs
@@ -2,35 +2,171 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Binned.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Logging;
+using Binned.Areas.Identity.Data;
+using System.Security.Claims;
+
+
 
 namespace Binned.Pages
 {
     public class LoginModel : PageModel
     {
+        private readonly SignInManager<BinnedUser> _signInManager;
+        private readonly ILogger<LoginModel> _logger;
+
+        public LoginModel(SignInManager<BinnedUser> signInManager, ILogger<LoginModel> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
         [BindProperty]
-        public Login LModel { get; set; }
+        public InputModel Input { get; set; }
 
-        private readonly SignInManager<IdentityUser> signInManager;
-        public LoginModel(SignInManager<IdentityUser> signInManager)
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public string? ReturnUrl { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [TempData]
+        public string? ErrorMessage { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public class InputModel
         {
-            this.signInManager = signInManager;
-        }
-        public void OnGet()
-        {
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            [EmailAddress]
+            public string? Email { get; set; }
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            [DataType(DataType.Password)]
+            public string? Password { get; set; }
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Display(Name = "Remember me?")]
+            public bool RememberMe { get; set; }
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task OnGetAsync(string? returnUrl = null)
         {
+            if (!string.IsNullOrEmpty(ErrorMessage))
+            {
+                ModelState.AddModelError(string.Empty, ErrorMessage);
+            }
+
+            returnUrl ??= Url.Content("~/");
+
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string? returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
             if (ModelState.IsValid)
             {
-                var identityResult = await signInManager.PasswordSignInAsync(LModel.Username, LModel.Password,
-                LModel.RememberMe, false);
-                if (identityResult.Succeeded)
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var user = await _signInManager.UserManager.FindByNameAsync(Input.Email);
+                if (user == null)
                 {
-                    return RedirectToPage("AccountProfile");
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return Page();
                 }
-                ModelState.AddModelError("", "Username or Password incorrect");
+
+                //var result = await _signInManager.CheckPasswordSignInAsync(user, Input.Password, false);
+
+                /* if (result.Succeeded)
+                 {
+                     var claims = new List<Claim>
+                     {
+                         new Claim("admin", "pwd"), // first string might be amr
+                         new Claim("EmployeeNumber", "1")
+                     };
+
+                     var roles = await _signInManager.UserManager.GetRolesAsync(user);
+
+                     if (roles.Any())
+                     {
+                         //"Manager,User"
+                         var roleClaim = string.Join(",", roles);
+                         claims.Add(new Claim("Roles", roleClaim));
+                     }
+
+                     await _signInManager.SignInWithClaimsAsync(user, Input.RememberMe, claims);
+
+                     _logger.LogInformation("User logged in.");
+                     return LocalRedirect(returnUrl);
+                 } */
+
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User logged in.");
+                    return LocalRedirect(returnUrl);
+                }
+                if (result.RequiresTwoFactor)
+                {
+                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
+                }
+                if (result.IsLockedOut)
+                {
+                    _logger.LogWarning("User account locked out.");
+                    return RedirectToPage("./Lockout");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return Page();
+                }
             }
+
+            // If we got this far, something failed, redisplay form
             return Page();
         }
     }
---
+++ Binned/Pages/Account/Manage/Dashboard.cshtml.cs
@@ -0,0 +1,140 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Serialization;
+using Binned.Areas.Identity.Data;
+
+namespace Binned.Pages.Account.Manage
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly UserManager<BinnedUser> _userManager;
+        private readonly SignInManager<BinnedUser> _signInManager;
+
+        public IndexModel(
+            UserManager<BinnedUser> userManager,
+            SignInManager<BinnedUser> signInManager)
+            
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public string Username { get; set; }
+        //public string Firstname { get; set; }
+        //public string Lastname { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [TempData]
+        public string StatusMessage { get; set; }
+
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public class InputModel
+        {
+            /*public class binnedAccountUser : IdentityUser
+            {
+                public string FirstName { get; set; }
+                public string LastName { get; set; }
+            }*/
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [DataType(DataType.Text), MinLength(1, ErrorMessage = "Enter at least 1 character."), MaxLength(255)]
+            [Display(Name = "FirstName")]
+            public string FirstName { get; set; }
+
+            [DataType(DataType.Text), MinLength(1, ErrorMessage = "Enter at least 1 character."), MaxLength(255)]
+            [Display(Name = "LastName")]
+            public string LastName { get; set; }
+        }
+
+        private async Task LoadAsync(BinnedUser user)
+        {
+            var userName = await _userManager.GetUserNameAsync(user);
+
+            Username = userName;
+
+            Input = new InputModel
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+           
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            if (Input.FirstName!= user.FirstName)
+            {
+                if (Input.FirstName != null)
+                {
+                    user.FirstName = Input.FirstName;
+                }            
+            }
+            if (Input.LastName != user.LastName)
+            {
+                if (Input.LastName != null) {
+                    user.LastName = Input.LastName;
+                }
+            }
+
+            await _userManager.UpdateAsync(user);
+
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Your profile has been updated";
+            return RedirectToPage();
+        }
+    }
+}
