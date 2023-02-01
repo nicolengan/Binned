f5943f025e8196d80ad7c8b01543e75281a427e9
sleepydip <fsnurulaini@gmail.com>
2023-02-02 02:49:16 +0800

    updated prod display

---
+++ Binned/Pages/User/productDisplay.cshtml.cs
@@ -0,0 +1,41 @@
+using Binned.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Binned.Model;
+
+namespace Binned.Pages.User
+{
+    public class productDisplayModel : PageModel
+    {
+        private readonly WishlistService _wishlistService;
+        private readonly CartService _cartService;
+        private readonly ProductService _productService;
+        public productDisplayModel(ProductService productService, CartService cartService, WishlistService wishlistService)
+        {
+            _wishlistService = wishlistService;
+            _cartService = cartService;
+            _productService = productService;
+        }
+        public List<Product> ProductList { get; set; } = new();
+
+        public Model.Product Product { get; set; }
+
+        public void OnGet()
+        {
+            ProductList = _productService.GetAll();
+        }
+
+        public async Task<IActionResult> OnPostAddToCartAsync(int productId)
+        {
+            await _cartService.AddItem("test", productId);
+            return RedirectToPage("/Cart");
+        }
+
+        public async Task<IActionResult> OnPostAddToWishlistAsync(int productId)
+        {
+            await _wishlistService.AddItem("test", productId);
+            return RedirectToPage("/User/Wishlist");
+        }
+    }
+
+}
