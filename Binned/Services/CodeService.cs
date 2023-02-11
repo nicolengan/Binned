a98e708692c739de1f496c56820407961c35f2a9
nic <nicolenganjiaxian@gmail.com>
2023-02-11 18:55:09 +0800

    added codeservice

--- Binned/Services/CodeService.cs
+++ Binned/Services/CodeService.cs
@@ -1,6 +1,32 @@
-﻿namespace Binned.Services
+﻿using Binned.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Binned.Services
 {
     public class CodeService
     {
+        private readonly MyDbContext _context;
+
+        public CodeService(MyDbContext context)
+        {
+            _context = context;
+        }
+        public List<PromoCode> GetAll()
+        {
+            return _context.PromoCodes
+                .OrderBy(d => d.Id)
+                .ToList();
+        }
+        public void AddOrder(PromoCode code)
+        {
+            _context.PromoCodes.Add(code);
+            _context.SaveChanges();
+        }
+
+        public void UpdateOrder(PromoCode code)
+        {
+            _context.PromoCodes.Update(code);
+            _context.SaveChanges();
+        }
     }
 }
